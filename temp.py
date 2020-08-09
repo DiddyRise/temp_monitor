dd4d6c2512287c6d789480ab9406bab79dd83656
DiddyRise <thorben.gilles@gmail.com>
2020-08-09 13:20:49 +0200

    Initial commit of script

---
+++ temp.py
@@ -0,0 +1,26 @@
+import Adafruit_DHT
+import sys
+import time
+from datetime import datetime
+
+#now = datetime.now()
+
+# seet your sensor
+sensor = Adafruit_DHT.AM2302
+# Set the GPIO pin (e.g. GPIO17 = 17)
+pin = 17
+
+f = open("results.csv", "w")
+f.write("Time,Temperature,Humidity\n")
+while True:
+    time.sleep(60)
+    now = datetime.now()
+    current_time = now.strftime("%H:%M")
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    if humidity is not None and temperature is not None:
+         result = str(current_time) + "," + str(round(temperature, 2)) + "," +  str(round(humidity, 2)) 
+         print(result)
+         f.write(result + "\n")
+    else:
+        print("0")
+        
