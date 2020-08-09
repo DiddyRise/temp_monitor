a98d92e8758dab738817061f77139b12e58c5ec3
DiddyRise <thorben.gilles@gmail.com>
2020-08-09 18:06:27 +0200

    final change for now - added some documentation

--- temp.py
+++ temp.py
@@ -3,18 +3,27 @@ import sys
 import time
 from datetime import datetime
 
-#now = datetime.now()
-
-# seet your sensor
+########################################
+#
+#Change your stuff here!
+#
+# set your sensor
+#Adafruit_DHT.AM2302 / Adafruit_DHT.11 / Adafruit_DHT.22
 sensor = Adafruit_DHT.AM2302
 # Set the GPIO pin (e.g. GPIO17 = 17)
 pin = 17
+#
+# Set the time in seconds between each probe - Please note that that the minimum is 2 seconds
+set_time = 300
+#
+########################################
+
 
 f = open("results.csv", "w")
 f.write("Time,Temperature,Humidity\n")
 f.close
 while True:
-    time.sleep(60)
+    time.sleep(set_time)
     f = open("results.csv", "a")
     now = datetime.now()
     current_time = now.strftime("%H:%M")
